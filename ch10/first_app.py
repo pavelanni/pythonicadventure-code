3f04372ea7b7a89d1c4257d03345d2ac607f8418
Pavel Anni <pavel.anni@sambanovasystems.com>
2022-10-17 10:22:37 -0400

    add ch10

---
+++ ch10/first_app.py
@@ -0,0 +1,25 @@
+# A simple web application.
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/hello/<name>")
+def greet(name="Stranger"):
+    return render_template("greeting.html", name=name)
+
+
+@app.route("/order", methods=("GET", "POST"))
+def order():
+    if request.method == "POST":
+        drink = request.form["drink"]
+        print("Drink: ", drink)
+    if request.method == "GET":
+        drink = ""
+
+    return render_template("forms.html", drink=drink)
