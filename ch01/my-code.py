1eacaf4e349516e27ddc27c19508ce166d427306
raul espina <respina@bigwise.com>
2024-08-05 20:02:50 -0400

    add: comments to first chapter

--- ch01/my-code.py
+++ ch01/my-code.py
@@ -1,12 +1,17 @@
 print("Wellcome to Raul'a Coffee Shop!")
 
+# Se puede pasar argumentos a la variable input() para que se muestre un mensaje al usuario
 name = input("Please enter your name: ")
 drink = input("Please enter your drink: ")
 flavor = input("Please enter your flavor: ")
 topping = input("Please enter your topping: ")
 
+# Es parecido al console.log() de JavaScript
+# Permite pasar varios argumentos separados por coma
 print("Here is your order, ", name)
 print("Main product: ", drink)
 print("Flavor: ", flavor)
 print("Topping: ", topping)
 print("Thanks for your order!")
+
+input("Press Enter to continue...")
