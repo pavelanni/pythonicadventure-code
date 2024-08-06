5c4b0bddacd78db5c9de43064b7373ec86199c97
raul espina <respina@bigwise.com>
2024-08-05 20:24:51 -0400

    feature: add code from the book

--- ch02/code.py
+++ ch02/code.py
@@ -3,3 +3,14 @@ flavors = ["caramel", "vanilla", "peppermint", "raspberry", "plain"]
 toppings = ["chocolate", "cinnamon", "caramel"]
 
 print(drinks)
+
+print("--------------------------------------------")
+print("Drinks:")
+i = 1
+for d in drinks:
+    print(i,d)
+    i += 1
+drinkIndex = int(input("Please select the drink by number: ")) - 1
+
+
+print("your selected drink is: ", drinks[drinkIndex])
