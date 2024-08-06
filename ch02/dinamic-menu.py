1a6b7cc7b62d5267353ac383f9b08f582edd7249
raul espina <respina@bigwise.com>
2024-08-05 20:44:32 -0400

    smart select created

--- ch02/dinamic-menu.py
+++ ch02/dinamic-menu.py
@@ -1,13 +1,28 @@
 import msvcrt
+import os
 
 options = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Exit']
 
 control = True
+currentOption = 0	# Default Current selected option
 while control:
+    os.system('cls')  # Clear the consol
     print("Select an option:")
     for i in range(len(options)):
-        print(i + 1, options[i])
-    print("Please select the option by number: ", end="")
+        if (currentOption == i) :
+            print("->", i + 1, options[i])
+        else:
+            print("  ", i + 1, options[i])
+    print("Move arrow keys to select, press Enter to confirm:")
     keypress = msvcrt.getch()
-    print(keypress)
-    input()
+    if keypress == b'\xe0':
+        keypress = msvcrt.getch()
+        if keypress == b'H':	# Up arrow
+            currentOption -= 1
+            if currentOption < 0:
+                currentOption = len(options) - 1
+        elif keypress == b'P':	# Down arrow
+            currentOption += 1
+            if currentOption >= len(options):
+                currentOption = 0
+    
